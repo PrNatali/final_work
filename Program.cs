8a19e6f80b90cc73c2ba5207cf3c4d12b20de93d
PrNatali <79377175544@ya.ru>
2022-10-31 13:40:16 +0300

    save program console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[]CreateArray(int size)
+{
+    string [] newArray = new string [size];
+    for(int i = 0; i<size; i++)
+    {
+        Console.Write("Enter string: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+void PrintArray(string[]arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + "  ");
+    Console.WriteLine();
+}
+void LessThreeChar(string[]arr)
+{
+    for(int i =0; i<arr.Length; i++)
+    {
+        int x = 3; // Значение из условия задачи
+        if(arr[i].Length <= x)
+            Console.Write(arr[i] + " , ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Enter quantity string: ");
+int len = Convert.ToInt32(Console.ReadLine());
+string[]myArr = CreateArray(len);
+PrintArray(myArr);
+LessThreeChar(myArr);
